9f2d2c73edde672fdf8b8d0849566144ebe37b28
Devedse <davydavidse@gmail.com>
2023-02-25 20:51:15 +0100

    Setup env for uploader

--- src/DLPR.LicensePlateData/DLPR.LicensePlateData.Uploader/Program.cs
+++ src/DLPR.LicensePlateData/DLPR.LicensePlateData.Uploader/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DLPR.LicensePlateData.Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace DLPR.LicensePlateData.Uploader
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+
+            Infrastructure.DependencyResolver.DependencyResolverService.Register(builder.Services, builder.Configuration);
+
+            var host = builder.Build();
+
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetService<DLPRLicensePlateDbContext>();
+
+                var curPlates = dbContext.LicensePlates.ToList();
+            }
+
+
+        }
+    }
+}
